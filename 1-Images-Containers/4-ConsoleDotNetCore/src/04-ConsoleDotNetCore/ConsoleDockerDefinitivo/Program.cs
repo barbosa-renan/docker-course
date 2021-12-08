d5d3449121b553bac420c39a517b4c1f68cd31e5
Renan Barbosa <renanvinicius.quadros@gmail.com>
2021-12-07 21:45:38 -0300

    Console .Net Docker

---
+++ 1-Images-Containers/4-ConsoleDotNetCore/src/04-ConsoleDotNetCore/ConsoleDockerDefinitivo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleDockerDefinitivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = Environment.GetEnvironmentVariable("user");
+            var password = Environment.GetEnvironmentVariable("password");
+
+            var debug = Environment.GetEnvironmentVariable("debug");
+
+            if (!string.IsNullOrWhiteSpace(debug))
+            {
+
+                Console.WriteLine($"ConnectionString {user}:{password}");
+
+                Console.WriteLine(Environment.MachineName);
+                Console.WriteLine(Environment.OSVersion);
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
